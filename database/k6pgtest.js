20af8228a775e1b4f615e5018959b18bbf1018c6
andrewbinkard <andrewbinkard@gmail.com>
2020-09-04 16:39:31 -0500

    optimize testing

--- database/k6pgtest.js
+++ database/k6pgtest.js
@@ -4,11 +4,17 @@ import { check, sleep } from "k6";
 export let options = {
   vus: 1,
   stages: [
-    { duration: "10s", target: 100 },
-    { duration: "10s", target: 250 },
-    { duration: "10s", target: 500 },
-    { duration: "10s", target: 250 },
-    { duration: "10s", target: 100 },
+    { duration: "30s", target: 100 },
+    { duration: "1m", target: 200 },
+    { duration: "1m", target: 300 },
+    { duration: "1m", target: 500 },
+    { duration: "1m", target: 750 },
+    { duration: "1m", target: 1000 },
+    { duration: "20m", target: 1000 },
+    { duration: "1m", target: 750 },
+    { duration: "1m", target: 500 },
+    { duration: "1m", target: 250 },
+    { duration: "1m", target: 100 },
   ],
 };
 // default
@@ -18,15 +24,25 @@ export let options = {
 // }
 
 // request for item by id w/ static id
-export default function () {
-  http.get("http://localhost:8008/items/9876784");
-}
+// export default function () {
+//   let res = http.get("http://localhost:8008/items/9876784");
+//   check(res, {
+//     "is status 200": (r) => r.status === 200,
+//   });
+//   // value passed in is in seconds, not ms
+//   sleep(0.5);
+// }
 
 // request for item by id w/ random id
-// export default function () {
-//   http.get(
-//     `http://localhost:8008/items/${Math.floor(Math.random() * 10000000) + 1}`
-//   );
-// }
+export default function () {
+  let res = http.get(
+    `http://localhost:8008/items/${Math.floor(Math.random() * 10000000) + 1}`
+  );
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
+  // value passed in is in seconds, not ms
+  sleep(0.5);
+}
 
 // request for item by name
